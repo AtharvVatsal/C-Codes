f5fb99185ccf89c9b275ecd3e8cdff62f908de81
Atharv Vatsal <atharvvatsal@outlook.com>
2024-02-21 11:07:09 +0530

    Hello World created

---
+++ HelloWorld.c
@@ -0,0 +1,7 @@
+#include <stdio.h> //includes standard input output library in the file
+
+int main()         //main function. Execution of program done within it
+{
+    printf("Hello World");  //printf function, belongs to stdio.h
+    return 0;
+}
