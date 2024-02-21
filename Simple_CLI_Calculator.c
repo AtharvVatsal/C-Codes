60a3a176482ff9b397ec4ea744cdb35cfd4323ff
Atharv Vatsal <atharvvatsal@outlook.com>
2024-02-21 18:55:50 +0530

    Simple CLI Calculator

---
+++ Simple_CLI_Calculator.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int cont,a,b;
+    printf("Enter First number: ");
+    scanf("%d",&a);
+    printf("Enter Second number: ");
+    scanf("%d",&b);
+    printf("Enter -->\n1 for Addition\n2 for Substraction\n3 for Multiplication\n4 for divison\nYour Choice <--> ");
+    scanf("%d",&cont);
+    switch (cont)
+    {
+    case 1:
+        printf("%d + %d = %d",a,b,a+b);
+        break;
+    case 2:
+        printf("%d - %d = %d",a,b,a-b);
+        break;
+    case 3:
+        printf("%d X %d= %d",a,b,a*b);
+        break;
+    case 4:
+        printf("%d / %d= %.2f",a,b,(float)a/b);
+        break;
+    default:
+        printf("Wrong Choice Entered. Re-Run Code");
+        break;
+    }
+    
+    return 0;
+}
