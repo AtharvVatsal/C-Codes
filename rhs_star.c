b63ba6c8a2394553245f54a490fcf3c0a9410a74
Atharv Vatsal <atharvvatsal@outlook.com>
2024-02-23 18:28:19 +0530

    RHS 90 deg  Star Pattern

---
+++ rhs_star.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*
+For n=5 ->
+
+*
+**
+***     This Is A Right Angled Start Triangle Pattern(Left sided)
+****
+*****
+
+*/
+int main()
+{
+    int n;
+    printf("Enter Number of rows & columns: ");
+    scanf("%d",&n);
+    printf("\n");
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<=i; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
