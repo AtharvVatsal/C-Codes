caeb731f5bca41e13d5ef93ef891e3635ca58024
Atharv Vatsal <atharvvatsal@outlook.com>
2024-02-24 23:40:09 +0530

    Matrix in C

--- matrix.c
+++ matrix.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-
+#define MAX 10
 int main()
 {
-    int mat[3][3],n;
-    for(int i = 0; i< 3; i++){
-        for (int j = 0; j < 3; j++)
+    int mat[MAX][MAX],rows,columns;
+    printf("Enter Number of Rows: ");
+    scanf("%d",&rows);
+    printf("Enter Number of Columns: ");
+    scanf("%d",&columns);
+    for(int i = 0; i< rows; i++){
+        for (int j = 0; j < columns; j++)
         {
             printf("Enter Element At [%d][%d] position : ",i+1,j+1);
             scanf("%d",&mat[i][j]);
         }
     }
-    for (int i = 0; i < 3; i++)
+    for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < columns; j++)
         {
             printf("%6d",mat[i][j]);
         }
