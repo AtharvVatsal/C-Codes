1653335f92026dc0c439a71fe62d5ecff5ef1814
Atharv Vatsal <atharvvatsal@outlook.com>
2024-02-25 00:28:43 +0530

    Factorial of a number

---
+++ factorial.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int Factorial(int number){
+    if (number < 0) //failsafe (does not work, looking into it)
+    {
+        printf("No factorial for negative numbers");
+        return -1;
+    } else{
+        if (number == 0 || number == 1)
+        {
+            return 1;
+        } else {
+            return number * Factorial(number - 1);
+        }
+    }
+    
+}
+
+int main()
+{
+    int n;
+    printf("Enter The Number: ");
+    scanf("%d", &n);
+    int fac = Factorial(n);
+    printf("Factorial of %d is %d", n, fac);
+    return 0;
+}
