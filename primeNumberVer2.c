24a133ff9f8b2d90b78e3c4de2e455eaee8d55e7
Atharv Vatsal <atharvvatsal@outlook.com>
2024-02-26 22:27:47 +0530

    Prime Number Version 2(Efficient) Final Code

--- primeNumberVer2.c
+++ primeNumberVer2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+
 /*
 k1 x k2 = n
 (k2 = n/k1)
@@ -7,29 +8,35 @@ k1 x k2 = n
 if k1 <= sqrt(n)
 Then k2 >= sqrt(n)
 */
+
 int main()
 {
     int n;
-    int isPrime =1; //Assumes the number entered is prime 
+    int isPrime = 1; // Assumes the number entered is prime
     printf("Enter The Number: ");
     scanf("%d", &n);
-    for (int i = 2; i < (int)sqrt(n); i++)
+
+    for (int i = 2; i <= (int)sqrt(n); i++)
     {
-        if (n%i == 0)
+        if (n % i == 0)
         {
             isPrime = 0;
+            break; // No need to continue checking if a divisor is found
         }
-        if (isPrime)
-        {
-            printf("The Number %d is prime!\n",n);
-        } else{
-            printf("The Number %d is composite!",n);
-        }
-        
     }
+
+    if (isPrime)
+    {
+        printf("The Number %d is prime!\n", n);
+    }
+    else
+    {
+        printf("The Number %d is composite!\n", n);
+    }
+
     return 0;
 }
+
 /*
 We get a pair of multipication is a number is prime so not to check twice root(n) is used.
 */
-//Only works for numbers > 9
